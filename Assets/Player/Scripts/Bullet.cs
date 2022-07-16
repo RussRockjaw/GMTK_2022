08cfbbd04cb86ee01d3f23cc7e1b3b6b4a9ad973
Russ <Pokerchips90@gmail.com>
2022-07-15 23:02:29 -0400

    Working player and gun

---
+++ Assets/Player/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]private float speed;
+    [SerializeField]Rigidbody rb;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void Activate()
+    {
+        this.gameObject.SetActive(true);
+        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
+        rb.AddTorque(new Vector3(Random.Range(-1, 1),Random.Range(-1, 1),Random.Range(-1, 1)) * 1000, ForceMode.VelocityChange);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        rb.useGravity = true;
+    }
+}
