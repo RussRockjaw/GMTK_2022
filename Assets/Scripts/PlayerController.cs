099475bdc00be9c748709140075b7f4e1fbe42cc
Kyle Ericson <kyle.ericson2@gmail.com>
2022-07-16 17:14:25 -0400

    Adding the shot dice to a list of dice to be used to scoring

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,12 +12,16 @@ public class PlayerController : MonoBehaviour
     private float horizontalRotation, verticalRotation;
     private float vert, hori;
     private Vector3 dir;
+    private List<GameObject> dice;
+    public Shahtzee shahtzee;
     
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
         health = 20;
+        dice = new List<GameObject>();
+        shahtzee = new Shahtzee();
     }
 
     void Update()
@@ -30,7 +34,9 @@ public class PlayerController : MonoBehaviour
         Aim();
         if(Input.GetMouseButtonDown(0))
         {
-            gun.Shoot();
+            GameObject d = gun.Shoot();
+            if(d != null)
+                dice.Add(d);
         } 
     }
 
@@ -43,12 +49,27 @@ public class PlayerController : MonoBehaviour
         cam.transform.localEulerAngles = new Vector3(verticalRotation, 0, 0);
     }
 
+    // TODO/feature: pick up should happen when looking, on click, not on just walking over it
     void OnCollisionEnter(Collision col)
     {
         if(col.gameObject.tag == "Dice")
-        {
-            gun.Reload(col.gameObject);
-        }
+            PickUp(col.gameObject);
+    }
+
+    // TODO/feature: this should open a menu of the score card
+    //               the player should be able to click the choice they want to score
+    //               with their currently laid out dice
+    //               all dice need to be on the ground (in the dice array)
+    //               to be able to trigger a CashIn
+    public void CashInDice()
+    {
+
+    }
+
+    public void PickUp(GameObject g)
+    {
+        dice.Remove(g);
+        gun.Reload(g);
     }
 
     public void UpdateHealth(int val)
